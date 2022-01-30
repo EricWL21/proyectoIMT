35d45499084e2e8962975ced14530d48e3667cd1
EricWL21 <98501554+EricWL21@users.noreply.github.com>
2022-01-29 19:52:06 -0600

    Update and rename deteccion_objetos.py to tomato_detection.py

--- tomato_detection.py
+++ tomato_detection.py
@@ -5,41 +5,41 @@ import serial
 com = 'COM4'
 baud = 9600
 ser = serial.Serial(com, baud)
-cap = cv2.VideoCapture(0)  #Creamos una variable para abrir la cámara
+cap = cv2.VideoCapture(0)  #Open camera, use 1 if using external webcam
 
-manzana_min = np.array([0, 112, 150],np.uint8)  ## HSV (tinte saturación y brillo) Variables con información de tonos de azul que queremos procesar
-manzana_max = np.array([21, 255, 255],np.uint8)  ##Variables con información de tonos de azul que queremos procesar
+min = np.array([0, 112, 150],np.uint8)  ## HSV (Hue, saturation and value) tones we intend to process
+max = np.array([21, 255, 255],np.uint8)
 #contador = 0
 while True:
-    ret, frame = cap.read()        #ret = la imagen se abrío correctamente, frame = La imagen que se está capturando
+    ret, frame = cap.read()        #ret = Image successfully opened, frame = Image captured
     frame = cv2.resize(frame,(500,500))
 
-    if ret:                        #Si la imagen se capturó correctamente entramos a este if
-        frame = cv2.flip(frame, 1) #-1 para wewbcam        #Reflejamos la imagen para tener una mejor perspetiva
-        frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)    #Convertimos el frame de RGB a HSV
+    if ret:                        #If image capture is successful
+        frame = cv2.flip(frame, 1) #-1 para wewbcam        #Flip image since we're using integrated webcam
+        frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)    #RGB to HSV conversion
         frameHSV = cv2.medianBlur(frameHSV, 51)
-        mascara = cv2.inRange(frameHSV, manzana_min, manzana_max)    #Creamos una mascara con esta función sobre el frame captado
+        mascara = cv2.inRange(frameHSV, min, max)    #We create a mask with previously defined HSV values
         mascara = cv2.morphologyEx(mascara, cv2.MORPH_OPEN, kernel=np.ones((9,9),np.uint8))
-        contornos, _ = cv2.findContours(mascara, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)   #buscamos contornos en la mascara que aacabamos de crear en tonos de verde
+        contornos, _ = cv2.findContours(mascara, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)   #We search contours in the created mask
         x = []
         y = []
 
-        for c in contornos:             #Hacemos un recorrido de los contornos que se encontraron
-            area = cv2.contourArea(c)    ##Calculamos el área de cada contorno que encontró
+        for c in contornos:             #We loop through all contours
+            area = cv2.contourArea(c)    ##We calculate the area of the contour
             if area > 1000:
-                M = cv2.moments(c)       ##Encontramos los momentos para calcular el centroide
+                M = cv2.moments(c)       ##We find the moments to determine the center of the contour
                 if M["m00"] == 0:
                      M["m00"] = 1
-                x.append(int(M["m10"] / M["m00"]) )     ##Estas son las ccordenadas x y y del centro de la figura detectada
+                x.append(int(M["m10"] / M["m00"]) )     ##x and y coordinates of the center of the contour
                 y.append(int(M['m01'] / M['m00']))
-                font = cv2.FONT_HERSHEY_SIMPLEX     ##Declaro el tipo de fuente
-                print(area)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                #print(area)
 
                 if len(x)==1:
-                    cv2.circle(frame, (x[0], y[0]), 7, (0, 0, 255),-1)  ##dibujamos un pequeño circulo rojo -1relleno, radio 7
-                    cv2.putText(frame, 'x = {}  y = {}'.format(x[0], y[0]), (20 ,400), font, 1, (0, 0, 255), 2, cv2.LINE_AA)    ##escribimos las coordenadas indicadas
-                    nuevoContorno = cv2.convexHull(c)     ##Une todos los puntos que encontro en una forma cerrada
-                    cv2.drawContours(frame, [nuevoContorno], 0, (255, 0, 0), 3)  ##Dibujamos el nuevo contorno encontrado
+                    cv2.circle(frame, (x[0], y[0]), 7, (0, 0, 255),-1)  ##We draw a circle around the contour
+                    cv2.putText(frame, 'x = {}  y = {}'.format(x[0], y[0]), (20 ,400), font, 1, (0, 0, 255), 2, cv2.LINE_AA)    ##We display the coordinates of the center
+                    nuevoContorno = cv2.convexHull(c)     ##We join all the points of the contour on a closed path
+                    cv2.drawContours(frame, [nuevoContorno], 0, (255, 0, 0), 3)  ##We draw the new contour
                     #ser.write(b"x{} y{}".format(x,y))
                 elif len(x)==2:
                     cv2.circle(frame, (x[0], y[0]), 7, (0, 0, 255),-1)  ##dibujamos un pequeño circulo rojo -1relleno, radio 7
